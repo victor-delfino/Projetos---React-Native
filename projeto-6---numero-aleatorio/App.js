3ed882f4840c8490ecd505f88b5ad068aa93f22a
victor-delfino <122889884+victor-delfino@users.noreply.github.com>
2025-04-04 18:04:10 -0300

    fix: app.js Projeto 6

--- projeto-6---numero-aleatorio/App.js
+++ projeto-6---numero-aleatorio/App.js
@@ -1,10 +1,10 @@
 import { View } from 'react-native';
 import GerarNumeroLogica from './src/components/GerarNumeroLogica';
-import styles from './src/pages/GerarNumero';
+
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <GerarNumeroLogica />
     </View>
   );
